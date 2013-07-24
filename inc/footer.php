d6e6af8474c33056e60373d551d9d0841f250262
Benjamin Staker <bassatcollege@gmail.com>
2013-07-24 20:16:07 +0100

    CDN for jQuery form was down - changed provider. Fixed linking to scripts file

--- inc/footer.php
+++ inc/footer.php
@@ -12,8 +12,8 @@
 	<!-- Placed at the end of the document so the pages load faster -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
-	<script src="//malsup.github.com/jquery.form.js"></script>
-	<script type='text/javascript' src="assets/js/script.js"></script>
+	<script src="//cdn.jsdelivr.net/jquery.form/3.36/jquery.form.min.js"></script>
+	<script type='text/javascript' src="assets/js/scripts.js"></script>
 	
 	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
 	<script type="text/javascript">
