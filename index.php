9c6a868ba5bc8a349fdc6a3f4627c0d139b75d9e
Benjamin Staker <bassatcollege@gmail.com>
2013-07-25 14:37:17 +0100

    WYSIWYG editor added

--- index.php
+++ index.php
@@ -3,11 +3,18 @@
 <h2> <a href="#" data-toggle="tooltip" class="link" title="" data-original-title="A European artistic and literary movement (1916-1923) that flouted conventional aesthetic and cultural values by producing works marked by nonsense, travesty, and incongruity">Dada</a> for the Twitter Age: The Size Of A Sales Receipt, And Edited By You</h2>
 
 <form action="vendor/twitteroauth/ajax.php" method="POST" class="form-signin">
-	<input name="search" type="text" class="input-block-level" placeholder="Search #">
+	<label for="search">#</label>
+	<input id="search" name="search" type="text" placeholder="Hashtag">
 	<button type="submit">Search</button>
 </form>
 
 <div id="results" class="hidden">
 	<ul></ul>
+</div>
+<div id="wysiwyg" class="hidden">
+	<ul id="sortable"></ul>
+	<p><a href="#" data-toggle="tooltip" data-placement="bottom" class="link" title="" data-original-title="Remember, DADA is based on nonsense, travesty, and incongruity... so have fun and drag and drop to you're hearts content!">What do I do?</a></p>
+	<button class="button" type="submit">Next</button>
+	<a href="./" class="button resetbutton">Reset</a>
 </div>
 <?php require_once('./inc/footer.php'); ?>
