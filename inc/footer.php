6d487534b5612169eedf2377b551fe60b90ff6d8
Benjamin Staker <bassatcollege@gmail.com>
2013-07-25 14:36:55 +0100

    Small changes to the footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,24 +1,30 @@
 				</div>
 			</div>
-			<footer class="span2 offset5">
+		</div>
+		<div class="row">
+			<footer class="span6 offset3">
 				<a href="whatisdada.html">Learn more about DADA</a>
 			</footer>
 		</div>
 	</div>
+	
 	<div id="midground"></div>
 	<div id="foreground"></div>
 	<!-- Le javascript
 	================================================== -->
 	<!-- Placed at the end of the document so the pages load faster -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="//cdn.jsdelivr.net/jquery.form/3.36/jquery.form.min.js"></script>
-	<script type='text/javascript' src="assets/js/scripts.js"></script>
-	
+	<script src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+
 	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
 	<script type="text/javascript">
 		_uacct = "UA-68528-29";
 		urchinTracker();
 	</script>
+
+	<script type='text/javascript' src="assets/js/scripts.js"></script>
+	
 	</body>
 </html>
